503c27a4b0f9b2e78c01c7dfea50a18d9e718fdc
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-26 15:28:55 -0800

    used gulp with src,pipe and dest

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 
-gulp.task('default');
+gulp.task('default', function(){
+  console.log('running default task');
+  gulp
+        .src('./src/**/*.js')
+        .pipe(gulp.dest('./build'))
+});
+
+
+gulp.task('delayed', function (done) {
+    setTimeout(function () {
+        console.log('Delayed by 2 seconds');
+        done();
+    }, 2000);
+});
