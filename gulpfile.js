f0abdf4ffcea25beb77a323ad21785223b4626c2
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-26 15:46:22 -0800

    used gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -23,4 +23,9 @@ gulp.task('minify', function () {
     return gulp.src('./src/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./build'))
-})
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/**/*.js', ['default']);
+    gulp.watch('./test/*.js', ['test']);
+});
