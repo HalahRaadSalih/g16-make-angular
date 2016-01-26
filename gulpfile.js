b26bfdb9c948c6c7b0b4aad530142f76d8e87bbe
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-26 15:37:42 -0800

    implemented gulp and lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
-gulp.task('default', function(){
-  console.log('running default task');
-  gulp
-        .src('./src/**/*.js')
-        .pipe(gulp.dest('./build'))
-});
+// gulp.task('default', function(){
+//   console.log('running default task');
+//   gulp
+//         .src('./src/**/*.js')
+//         .pipe(gulp.dest('./build'))
+// });
+//
+//
+// gulp.task('delayed', function (done) {
+//     setTimeout(function () {
+//         console.log('Delayed by 2 seconds');
+//         done();
+//     }, 2000);
+// });
 
+gulp.task('default', ['lint']);
 
-gulp.task('delayed', function (done) {
-    setTimeout(function () {
-        console.log('Delayed by 2 seconds');
-        done();
-    }, 2000);
+gulp.task('lint', function(){
+  return gulp.src('./**/*.js')
+              .pipe(eslint())
+              .pipe(eslint.format())
+              .pipe(eslint.failAfterError());
 });
