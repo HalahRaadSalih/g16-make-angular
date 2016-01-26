e80cf268bb29d2faf85e895246ef3f34c33026f0
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-26 15:43:04 -0800

    implemented code minify with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
+var uglify = require('gulp-uglify');
 
 
 
-gulp.task('default', ['test']);
+gulp.task('default', ['minify']);
 
 gulp.task('lint', function(){
   return gulp.src('./**/*.js')
@@ -16,4 +17,10 @@ gulp.task('lint', function(){
 gulp.task('test', function () {
     return gulp.src('./test/*.js', {read: false})
         .pipe(mocha())
+});
+
+gulp.task('minify', function () {
+    return gulp.src('./src/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build'))
 })
